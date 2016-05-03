db7814072a828e32af14b31188aa9ddbedbef044
Schuyler Duveen <sky@skyb.us>
2016-05-03 14:30:12 -0400

    have non-minified version, too

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const concat = require("gulp-concat");
 
 gulp.task('default', () => {
   return gulp.src('./src/*.js')
+    .pipe(concat('ak-js.js'))
+    .pipe(gulp.dest('./build/'))
     .pipe(uglify())
     .pipe(concat('ak-js.min.js'))
     .pipe(gulp.dest('./build/'));
