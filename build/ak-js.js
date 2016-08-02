9f5a4a9dc77a756006efcfc9fe593191f80d1e27
Scott Reynen <scott.reynen@moveon.org>
2016-08-02 10:03:25 -0600

    include updates missing from previous commit

--- build/ak-js.js
+++ build/ak-js.js
@@ -379,56 +379,61 @@ window.onerror = function(errorMessage) {
   window.jsErrors[window.jsErrors.length] = errorMessage;
 }
 
-// Roster toggle-all checkbox
-function toggleSignups() {
+// Prepare toglle-all checkboxes
+function toggleAll() {
     $(this)
         .closest('form')
         .find('input.toggle')
         .attr('checked', this.checked);
 }
+
 // Update "N attendee(s)" in e-mail form
-function handleSignupsChanged() {
-    var frm = $(this).closest('form');
+function handleListChanged() {
+    var $form = $(this).closest('form');
     var to_count = $('input.toggle:checked').length;
-    // Mailing all recipients if nobody's checked
-    if ( !to_count )
-        to_count = $('input.toggle').length
-    // They've figured out the checkboxes; no need to advertise
-    if ( to_count )
-        frm.find('.check-recipients-help').hide();
-    frm.find('.to-count').text(to_count);
+    // Mailing all recipients if none are checked
+    if (to_count == 0) {
+        to_count = $('input.toggle').length;
+    }
+    else {
+        $form.find('.check-recipients-help').hide();
+    }
+    $form.find('.to-count').text(to_count);
 }
 // E-mail button
-function handleEmail() {
-    var frm = $(this).closest('form');
-    frm.find('.signup-list-controls').slideUp('fast');
-    frm.find('.contact-form').slideDown('fast');
+function showEmailForm() {
+    var $form = $(this).closest('form');
+    $form.find('.roster-controls').slideUp('fast');
+    $form.find('.contact-form').slideDown('fast');
     return false;
 }
+
 // "E-mail attendee", "e-mail cohost", "invite friends" links
 // Unhide if needed and fake :target on IE
 function handleJumpLink() {
     var targetEl;
     if ( this.id == 'email-cohosts-link' )
-        targetEl = $('fieldset.contact-cohosts');
+        targetEl = $('.contact-cohosts');
     else if ( this.id == 'email-attendees-link' )
-        targetEl = $('fieldset.contact-attendees');
+        targetEl = $('.contact-attendees');
     else if ( this.id == 'invite-friends-link' )
         targetEl = $('#taf');
     // Unhide e-mail form if needed
     if ( /^email-/.test(this.id) ) {
         var emailButton = targetEl.closest('form').find('input[type="submit"].email');
-        handleEmail.apply(emailButton, []);
+        showEmailForm.apply(emailButton, []);
     }
     // Highlight
     targetEl.addClass('target');
     // Allow jump to #foo to happen
     return true;
 }
-// Confirm cancelling event
+
+// Confirm a click
 function confirmSubmit() {
-    return confirm($(this).attr('confirm-message'));
+    return confirm($(this).attr('data-confirm-message'));
 }
+
 // Stash the name of the submit button in the 'action' form field
 // (helps the validation JS see which kind of action to validate for)
 function setFormAction() {
@@ -436,6 +441,7 @@ function setFormAction() {
     if ( !formEl && !formEl.action ) return;
     formEl.action.value = this.name;
 }
+
 // Check for required fields for remove/promote/demote/e-mail
 function validateRoster(form) {
     var action = form.action.value;
@@ -450,6 +456,7 @@ function validateRoster(form) {
         actionkit.errors['body:missing'] =
             actionkit.forms.errorMessage('event_contact_body:missing');
 }
+
 // Confirm removing folks
 function confirmRoster(form) {
     var action = form.action.value;
@@ -460,12 +467,16 @@ function confirmRoster(form) {
     else
         return true;
 }
+
 // Click anywhere in row to check/uncheck attendee
 function toggleRow(e) {
     var toggleEl = $(this).closest('tr').find('.toggle')[0];
-    if ( toggleEl ) toggleEl.checked = !toggleEl.checked;
+    if (toggleEl) {
+        toggleEl.checked = !toggleEl.checked;
+    }
     return false;
 }
+
 // Update the confirmation div from a different page
 function updateConfirmationMessage() {
     for (var key in actionkit.args) {
@@ -475,24 +486,18 @@ function updateConfirmationMessage() {
         }
     }
 }
-// Set everything up onready
-function initHostTools() {
-    $('input.toggle-all').click(toggleSignups);
-    $('input.toggle').click(handleSignupsChanged);
-    $('.signup-list td:not(.toggle-col)').click(toggleRow);
-    $('input[type="submit"].email').click(handleEmail);
+
+$(document).ready(function() {
+    $('.if-js').show();
+    $('a[data-confirm-message]').click(confirmSubmit);
+    $('input.toggle-all').click(toggleAll);
+    $('.event-roster td:not(.toggle-col)').click(toggleRow);
+    $('input[type="submit"].email').click(showEmailForm);
     $('input[type="submit"]').click(setFormAction);
     $('.jump-link').click(handleJumpLink);
-    $('a[confirm-message]').click(confirmSubmit);
-    if ($('#manage-host').length)
-        actionkit.forms.initValidation('manage-host');
-    if ($('#manage-attendee').length)
-        actionkit.forms.initValidation('manage-attendee');
-    $(window).load(function() {
-        updateConfirmationMessage();
-    });
-}
-$(initHostTools);
+    $('input.toggle').click(handleListChanged);
+    updateConfirmationMessage();
+});
 
 // From http://snipplr.com/view/43646/prepopulate-form-with-values-from-querystring/
 $(document).ready(function() {
