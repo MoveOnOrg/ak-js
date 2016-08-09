54584c9c2dc6a2f179ed758fdb0f01694f0d8c38
Scott Reynen <scott.reynen@moveon.org>
2016-08-09 07:05:20 -0600

    Remove stray retired_checkbox.min.js, add click_hide.js

--- build/ak-js.js
+++ build/ak-js.js
@@ -207,6 +207,19 @@ IN THE SOFTWARE.
 
 }).call(this);
 
+/**
+ * Set the data-hide attribute of a clickable element to a selector of elements
+ * that should be hidden on click. For example, this link should hide itself:
+ * <a href="#" id="hide-self" data-hide="#hide-self">Hide me</a>
+ */
+
+$(document).ready(function() {
+  $('a[data-hide]').click(function() {
+    $($(this).attr('data-hide')).hide();
+    return true;
+  });
+});
+
 $(document).ready(function() {
 
   /**
