cf0892abe6e802e3c21d825fc1683ac372e5fb62
Schuyler Duveen <sky@skyb.us>
2016-09-08 19:53:52 -0400

    mobile form: https://app.asana.com/0/153976252167165/154173162453769

--- build/ak-js.js
+++ build/ak-js.js
@@ -2106,6 +2106,86 @@ var Kicksend = {
 	};
 }));
 
+/*
+  Interacts with user_form.html
+  Use Cases:
+  * on filling out phone field with 10 digits, rolls down a SMS subscribe checkbox
+    -- checkbox should be default-checked, with phone_type="mobile"
+    -- unchecking the box will remove phone_type="mobile"
+  * for purposes of "pure phone subscription" have a survey form that 
+    -- hides email address completely and inserts a fake email address on submit
+    -- if fake email, also include suppress_subscribe="1"
+  $('#id_sms_subscribe').attr('data-phonemobile') has escalating values of:
+    - opt-in
+    - opt-out
+    - opt-out-email-optional
+    - opt-out-email-hidden
+
+  TO CONSIDER:
+    * should I implement this with EmailSuggestion (see donate form)
+    * how to hook in to form submission
+*/
+
+var SMS_SUBSCRIBE_DIV = '#id_subscribe_sms';
+
+//onLOAD
+(function() {
+  var phonemobile = ($(SMS_SUBSCRIBE_DIV).attr('data-phonemobile') || '');
+  //hide email field?
+  if (/opt-out-email-/.test(phonemobile)) {
+    if (/opt-out-email-hidden/.test(phonemobile)) {
+      $('#id_email_box').hide();
+      $('#id_sms_subscribed').change(function() {
+        if (!this.checked) {
+          //if they are not signing up by SMS, then we should show the email field
+          $('#id_email_box').show();
+        }
+      });
+    }
+    //HACK: monkeypatch/filter actionkit.forms.required()
+    var oldrequired = actionkit.forms.required;
+    actionkit.forms.required = function() {
+      var required_fields = oldrequired();
+      return $(required_fields).filter(function() {return (this != 'email')});
+    }
+  }
+})();
+
+var mobilePhoneUpdate = function() {
+  if ($(this).val().replace(/\D/g, '').length >= 10) {
+    var phonemobile = ($(SMS_SUBSCRIBE_DIV).attr('data-phonemobile') || '');
+    if (/opt-out/.test(phonemobile)) {
+      $('#id_sms_subscribed').prop('checked', true);
+      $('#id_sms_subscribed').val('sms_subscribed');
+    }
+    $(SMS_SUBSCRIBE_DIV).show(); //todo: css for slide down
+  }
+}
+
+//onCHANGE
+$('input[name=phone],input[name=mobile_phone]')
+  .on('change unfocus', mobilePhoneUpdate)
+  .each(mobilePhoneUpdate);
+
+
+//onSUBMIT
+$('form[name=act]').on('actionkitbeforevalidation', function() {
+  var mobile = $('input[name=phone],input[name=mobile_phone]').val().replace(/\D/g, '');
+  var mobile_subscribe = $('#id_sms_subscribed', SMS_SUBSCRIBE_DIV).prop('checked');
+
+  if (mobile_subscribe && mobile && mobile.length >= 10) {
+    if ($('#id_email').val() === '') {
+      $('#id_suppress_subscribe').val('1');
+      $('#id_email').val(mobile+'-smssubscriber@example.com');
+      $('#id_email_box').hide();
+    }
+
+    $('#id_sms_subscribed').val('sms_subscribed');
+    $('#id_sms_termsandconditions').val('sms_termsandconditions');
+    $('#id_robodial_termsandconditions').val('yes');
+  } 
+});
+
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex ;
 
--- src/phone_sms_terms.js
+++ src/phone_sms_terms.js
@@ -24,49 +24,56 @@ var SMS_SUBSCRIBE_DIV = '#id_subscribe_sms';
 (function() {
   var phonemobile = ($(SMS_SUBSCRIBE_DIV).attr('data-phonemobile') || '');
   //hide email field?
-  if (/opt-out-email-hidden/.test(phonemobile)) {
-    $('#id_email_box').hide();
+  if (/opt-out-email-/.test(phonemobile)) {
+    if (/opt-out-email-hidden/.test(phonemobile)) {
+      $('#id_email_box').hide();
+      $('#id_sms_subscribed').change(function() {
+        if (!this.checked) {
+          //if they are not signing up by SMS, then we should show the email field
+          $('#id_email_box').show();
+        }
+      });
+    }
+    //HACK: monkeypatch/filter actionkit.forms.required()
+    var oldrequired = actionkit.forms.required;
+    actionkit.forms.required = function() {
+      var required_fields = oldrequired();
+      return $(required_fields).filter(function() {return (this != 'email')});
+    }
   }
 })();
 
-//onCHANGE
-$('input[name=phone],input[name=mobile_phone]').on('change', function() {
-  if ((this).val().replace(/\D/g, '').length >= 10) {
+var mobilePhoneUpdate = function() {
+  if ($(this).val().replace(/\D/g, '').length >= 10) {
     var phonemobile = ($(SMS_SUBSCRIBE_DIV).attr('data-phonemobile') || '');
     if (/opt-out/.test(phonemobile)) {
-      $(SMS_SUBSCRIBE_DIV).attr('checked', 'checked');
+      $('#id_sms_subscribed').prop('checked', true);
+      $('#id_sms_subscribed').val('sms_subscribed');
     }
-    $(SMS_SUBSCRIBE_DIV).show();
+    $(SMS_SUBSCRIBE_DIV).show(); //todo: css for slide down
   }
-})
+}
+
+//onCHANGE
+$('input[name=phone],input[name=mobile_phone]')
+  .on('change unfocus', mobilePhoneUpdate)
+  .each(mobilePhoneUpdate);
+
 
 //onSUBMIT
 $('form[name=act]').on('actionkitbeforevalidation', function() {
-});
+  var mobile = $('input[name=phone],input[name=mobile_phone]').val().replace(/\D/g, '');
+  var mobile_subscribe = $('#id_sms_subscribed', SMS_SUBSCRIBE_DIV).prop('checked');
 
-
-  //onchange
-  if ($('#mobile_phone').val().replace(/\D/g, '').length >= 10) {
-      $('#id_sms_subscribed').attr('checked', 'checked');
-      $('.subscribe-sms-conditions').show();
-  }
-
-  //onsubmit
-  $('form#formSelection').submit(function (e) {
-    var mobile = $('#mobile_phone').val().replace(/\D/g, '');
-    if (mobile && mobile.length >= 10) {
-      if ($('#email').val() === '') {
-        $('#id_suppress_subscribe').val('1');
-        $('#email').val(mobile+'-smssubscriber@example.com');
-      }
-      //$('#id_sms_subscribed').val('sms_subscribed');
-      $('#id_sms_termsandconditions').val('sms_termsandconditions');
-      $('#id_robodial_termsandconditions').val('yes');
-    } 
-    if (($('#email').val() === '' && $('#mobile_phone').val() === '')
-        || $('#user_name').val() === '' || $('#signup-zip').val() === '') {
-      e.preventDefault();
-      $('span#please-fill-out').removeClass('hide');
-      return !1
+  if (mobile_subscribe && mobile && mobile.length >= 10) {
+    if ($('#id_email').val() === '') {
+      $('#id_suppress_subscribe').val('1');
+      $('#id_email').val(mobile+'-smssubscriber@example.com');
+      $('#id_email_box').hide();
     }
-  });
+
+    $('#id_sms_subscribed').val('sms_subscribed');
+    $('#id_sms_termsandconditions').val('sms_termsandconditions');
+    $('#id_robodial_termsandconditions').val('yes');
+  }
+});
