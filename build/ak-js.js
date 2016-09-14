194fe53ebe1d8233fa8773d8153dca5923d3a22f
Schuyler Duveen <sky@skyb.us>
2016-09-14 13:10:08 -0400

    mobile sms: oh, i guess we need both of these https://app.asana.com/0/153976252167165/154173162453769

--- build/ak-js.js
+++ build/ak-js.js
@@ -2148,6 +2148,13 @@ var SMS_SUBSCRIBE_DIV = '#id_subscribe_sms';
     // This js file should be loaded after that in wrapper.html
     // It doesn't matter if init/setupForm is run multiple times, because this hacked the dom element itself
     window.actionkit.form.email = false;
+    //HACK2: monkeypatch/filter actionkit.forms.required() -- neither one is sufficient by itself
+    var oldrequired = actionkit.forms.required;
+    actionkit.forms.required = function() {
+      var required_fields = oldrequired();
+      return $(required_fields).filter(function() {return (this != 'email')});
+    }
+
   }
 })();
 
